408ceb6fe6d3ac1e89a2bf800ec77434b0009986
ymzkjpx <ymzkjpx@gmail.com>
2021-09-07 00:14:19 +0900

    first commit

---
+++ src/main/java/example/explanationofpsystemsekkei/ExplanationOfPSystemsekkeiApplication.java
@@ -0,0 +1,13 @@
+package example.explanationofpsystemsekkei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExplanationOfPSystemsekkeiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExplanationOfPSystemsekkeiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/example/explanationofpsystemsekkei/ExplanationOfPSystemsekkeiApplicationTests.java
@@ -0,0 +1,13 @@
+package example.explanationofpsystemsekkei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExplanationOfPSystemsekkeiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
